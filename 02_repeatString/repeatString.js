eaecb3baa3d187c3213f9d85e730ef88d21cced6
ijaz94 <109833495+ijaz94@users.noreply.github.com>
2024-04-18 03:35:59 +0500

    using while loop and times decremented

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, times) {
+    let repeatedStringNum = "";
+    while (times > 0) {
+      repeatedStringNum += string;
+      times--;
+    }
+    return repeatedStringNum;
+  }
+  repeatString("abc", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -6,5 +6,5 @@ const repeatString = function (word, times) {
   }
   return string;
 };
-
+console.log(repeatString("hey", 3));
 module.exports = repeatString;
