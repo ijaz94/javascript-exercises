ffe5e742633e3edb0f1deaf68908de3d95e83f8c
Ijaz Ahmad <109833495+ijaz94@users.noreply.github.com>
2024-05-30 01:07:32 +0500

    My solution to the problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  const newArray = arr.map((element) => element.title);
+  return newArray;
 };
 
 // Do not edit below this line
