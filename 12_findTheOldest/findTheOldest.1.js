eb25fb36cd61bb7febc2724fe3f8dc6d86a52e22
Ijaz Ahmad <109833495+ijaz94@users.noreply.github.com>
2024-05-30 03:56:12 +0500

    my solution

--- 12_findTheOldest/findTheOldest.1.js
+++ 12_findTheOldest/findTheOldest.1.js
@@ -1,3 +1,5 @@
+const { getAge } = require("./findTheOldest");
+
 const findTheOldest = function (array) {
   return array.reduce((oldest, currentPerson) => {
     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
@@ -5,16 +7,6 @@ const findTheOldest = function (array) {
       currentPerson.yearOfBirth,
       currentPerson.yearOfDeath
     );
-    return oldestAge < currentAge ? currentPerson : oldest;
   });
 };
-
-const getAge = function (birth, death) {
-  if (!death) {
-    death = new Date().getFullYear();
-  }
-  return death - birth;
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+exports.findTheOldest = findTheOldest;
