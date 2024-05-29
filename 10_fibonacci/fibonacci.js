f53b4448840317a9618ed589b045e8193df6f964
Ijaz Ahmad <109833495+ijaz94@users.noreply.github.com>
2024-05-30 00:48:49 +0500

    my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+  let count;
+  if (typeof countArg !== "number") {
+    count = parseInt(countArg);
+  } else {
+    count = countArg;
+  }
+  if (count < 0) {
+    return "OOPS";
+  }
+  if (count == 0) {
+    return 0;
+  }
 
+  let fristPrev = 1;
+  let secondPrev = 0;
+  for (let i = 2; i <= count; i++) {
+    let current = fristPrev + secondPrev;
+    secondPrev = fristPrev;
+    fristPrev = current;
+  }
+  return fristPrev;
 };
 
 // Do not edit below this line
